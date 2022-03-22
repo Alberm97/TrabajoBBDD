deecc2901d8137e64d4497c811012aef3211f4ef
Alberm97 <92427844+Alberm97@users.noreply.github.com>
2022-03-22 13:32:27 +0100

    Add files via upload

--- Videoclub.sql
+++ Videoclub.sql
@@ -3,17 +3,20 @@ go
 use videoclub
 
 CREATE TABLE Peliculas(
-    ID int IDENTITY(1,1) primary key,
+    
     Títulos varchar(200),
-    Año_de_lanzamiento int,
-    Duracion VARCHAR (10),
+    Anyo_lanzamiento varchar(200),
+    Duracion varchar(200),
     Genero varchar(200),
-    IMDB_puntuacion INT,
     Director varchar(30),
-    Actor varchar(20),
-    Actor2 varchar(20),
-    Actor3 varchar(20),
-    Actor4 varchar(20),
+    Actor1 varchar(100),
+    Actor2 varchar(100),
+    Actor3 varchar(100),
+    Actor4 varchar(100),
 )
+drop table Peliculas
 
-BULK INSERT Peliculas FROM 
+BULK INSERT Peliculas FROM '/VAR/OPT/MSSQL/BACKUP/Videoclub/Videoclub.csv'
+WITH(
+    FIELDTERMINATOR = ',' , ROWTERMINATOR = '0X0a'
+)
